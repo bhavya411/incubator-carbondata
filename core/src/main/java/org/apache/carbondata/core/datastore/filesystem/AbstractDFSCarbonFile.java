edcfa7bbeb3ac7e17164968d2f05171ddfb64412
Bhavya <bhavya@knoldus.com>
2018-01-04 13:51:36 +0530

    Fixed for rebase

--- core/src/main/java/org/apache/carbondata/core/datastore/filesystem/AbstractDFSCarbonFile.java
+++ core/src/main/java/org/apache/carbondata/core/datastore/filesystem/AbstractDFSCarbonFile.java
@@ -23,6 +23,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
 import java.io.OutputStream;
 
 import org.apache.carbondata.common.logging.LogService;
